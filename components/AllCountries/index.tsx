991f886f378933005f9662739bcee7f4863457a6
Sriram Chamarthy <sriram2491999@gmail.com>
2022-08-27 08:14:21 +0530

    feat: added search box

--- components/AllCountries/index.tsx
+++ components/AllCountries/index.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { SearchBox } from "../SearchBox";
 
 export const AllCountries = () => {
   return (
     <div className="flex flex-1 h-full bg-transparent dark:bg-darkBlue">
-      Body
+      <div className="m-10">
+        <SearchBox />
+      </div>
     </div>
   );
 };
---
+++ components/SearchBox/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+
+export const SearchBox = () => {
+  return (
+    <div className="flex flex-row p-2 items-center rounded-md shadow-[0px_0px_5px_rgba(0,0,0,0.25)]">
+      <label htmlFor="search-box" className="px-4">
+        <AiOutlineSearch
+          size={20}
+          className="text-gray-500 dark:text-gray-300"
+        />
+      </label>
+
+      <input
+        className="min-w-[300px] dark:text-gray-300 dark:placeholder:text-gray-400 bg-transparent focus:outline-none"
+        id="search-box"
+        placeholder="Search for a country..."
+      />
+    </div>
+  );
+};
