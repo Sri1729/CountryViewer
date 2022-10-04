889a1c103f836f3707a6053f545cb124c9e76a20
Sri1729 <39293561+Sri1729@users.noreply.github.com>
2022-10-04 22:05:33 +0000

    Deploying to gh-pages from @ Sri1729/CountryViewer@5b5d9d422de44896a989806dc96d0d151043f3fc 🚀

---
+++ _next/static/DwosO-msy6v61DHw6OMUr/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-b1c0f21300825a43.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],"/details":["static/chunks/pages/details-2fae30bd488f6934.js"],sortedPages:["/","/_app","/_error","/details"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
