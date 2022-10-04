15eece5a08e095d9cdd36d99af0644d8092b2a2b
Sriram Chamarthy <sriram2491999@gmail.com>
2022-10-05 04:14:05 +0530

    fix: sticky header not working correctly

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,10 +7,10 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RootStoreProvider hydrationData={pageProps.hydrationData}>
       <div className="flex flex-1 flex-col h-full">
-        <div className="sticky top-0 z-10">
+        <div className="fixed w-full top-0 z-10">
           <Header />
         </div>
-        <div className="flex flex-1 p-10 bg-white dark:bg-darkerBlue">
+        <div className="relative flex flex-1 p-10 bg-white dark:bg-darkerBlue top-[68px] sm:top-[72px">
           <Component {...pageProps} />
         </div>
       </div>
