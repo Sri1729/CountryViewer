5607b3b32544b24e7a3267bd0aedaf7195c0bd75
Sri1729 <Sri1729@users.noreply.github.com>
2022-10-04 23:04:52 +0000

    Deploying to gh-pages from @ Sri1729/CountryViewer@452177b5a5f0e2fbb404d7cefdd16d5f81524d8b 🚀

---
+++ _next/static/TyPaRG36g_KxeWnTKLfqc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-00faf2a98d34582b.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],"/details":["static/chunks/pages/details-38f1370aae99280b.js"],sortedPages:["/","/_app","/_error","/details"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
--- _next/static/fxMtNpxleFbPdx1-9ffSX/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-6069df65bd3948d0.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],"/details":["static/chunks/pages/details-480b0b4a0cca195a.js"],sortedPages:["/","/_app","/_error","/details"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
